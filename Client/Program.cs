e5eae2ccd793d6b593ccb7416a5082ad27fceefd
Stonie <andrew.stone@drivenlogic.com.au>
2014-07-08 16:16:56 +1000

    rough cut

---
+++ Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        private const int PORT_NUMBER = 49000; // 1024 - 49151 : http://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+        private static UdpClient udpClient;
+
+        private static void Main(string[] args)
+        {
+            udpClient = new UdpClient(PORT_NUMBER);
+            udpClient.EnableBroadcast = true;
+     
+            Console.WriteLine("listening for messages...");
+            BeginReceive();
+
+            Console.WriteLine("Client Main done.");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// UDP receive call-back
+        /// </summary>
+        /// <param name="asyncResult"></param>
+        private static void ReceiveCallback(IAsyncResult asyncResult)
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, PORT_NUMBER);
+
+            byte[] bytes = udpClient.EndReceive(asyncResult, ref ipEndPoint);
+            
+            string message = Encoding.ASCII.GetString(bytes);
+            Console.WriteLine("From {0} received: {1} ", ipEndPoint.Address, message);
+
+            // keep reciving... 
+            BeginReceive();
+        }
+
+        private static void BeginReceive()
+        {
+            udpClient.BeginReceive(new AsyncCallback(ReceiveCallback), new object());
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Timers;
+
+namespace Server
+{
+    class Program
+    {
+        private const int PORT_NUMBER = 49000; // 1024 - 49151 : http://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+        private static Timer broadcastTimer;
+
+        private static void Main(string[] args)
+        {
+            broadcastTimer = new System.Timers.Timer(5000); // real value much bigger - client to store / cache.
+            broadcastTimer.Elapsed += OnTimedEvent;
+            broadcastTimer.Enabled = true;
+
+            Console.WriteLine("Server Main done.");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Timer elapsed handle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Console.Write("Timer elapsed...");
+            broadcast();
+        }
+
+        /// <summary>
+        /// Broadcast our local IP.
+        /// </summary>
+        private static void broadcast()
+        {
+            UdpClient udpClient = new UdpClient();
+            udpClient.EnableBroadcast = true;
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse("255.255.255.255"), PORT_NUMBER);
+
+            string message = "FTM @ " + LocalIpAddress();
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+
+            using (udpClient)
+            {
+                udpClient.Send(bytes, bytes.Length, ip);
+                udpClient.Close();
+            }
+
+            Console.WriteLine("Sent: {0} ", message);
+        }
+
+        /// <summary>
+        /// get the Ip of the first network adaptor
+        /// </summary>
+        /// <returns></returns>
+        private static  IPAddress LocalIpAddress()
+        {
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+                return host
+                    .AddressList
+                    .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            }
+
+            return null;
+        }
+    }
+}
